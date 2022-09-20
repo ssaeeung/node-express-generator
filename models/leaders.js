2f4dcc30538032302a1d5f61a8ca8e7ea251c90e
Torn <ajaree.eatlab.space>
2022-09-19 21:56:41 -0700

    Add basic authentication

---
+++ models/leaders.js
@@ -0,0 +1,35 @@
+const { model } = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose)
+const Currency = mongoose.Types.Currency
+
+const LeaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', LeaderSchema);
+
+module.exports = Leaders;
