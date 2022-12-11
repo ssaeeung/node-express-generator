d1c7bd00e2bb71fac84ebe1771280e8c407b1198
Torn <ajaree.eatlab.space>
2022-12-11 10:31:10 -0800

    imageUpload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var usersRouter = require('./routes/user');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose')
 const Dishes = require('./models/dishes')
@@ -62,6 +63,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
